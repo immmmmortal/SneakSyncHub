7b4290f8c7ce24e2d366a936a11870ed05be6f82
Illia Wolkow <kuza617@gmail.com>
2024-08-20 14:49:59 +0300

    feat: fixed sidebar accomplished

---
+++ frontend/src/app/components/default_view.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+const DefaultViewComponent = () => {
+    const numberOfItems = 3;
+
+    return (
+        <>
+            <div className="relative flex-grow">
+                <ul className="relative blur-xl flex-grow flex-col flex flex-wrap gap-4">
+                    {Array.from({length: numberOfItems}).map((_, index) => (
+                        <li
+                            key={index}
+                            className={`bg-sneakers-first h-56 min-h-fit rounded-2xl items-start flex flex-grow flex-row mt-5 text-ellipsis overflow-hidden relative`}>
+                            <span
+                                className="w-56 blur-sm flex bg-gray-600 self-center h-56 animate-pulse"></span>
+                            <div
+                                className="flex flex-col h-auto p-3 min-h-[228px] flex-grow ml-4">
+                                <div className="flex flex-row">
+                                    <div
+                                        className={`text-lg hover:text-orange-500 transition-all  w-fit overflow-hidden text-ellipsis h-8 duration-200 flex flex-grow font-semibold`}>
+                                        <span
+                                            className="flex w-4/5 rounded bg-gray-600 blur-sm animate-pulse">
+                                        </span>
+                                    </div>
+                                </div>
+                                <div className="text-md text-green-400">
+                                    <span
+                                        className="flex w-1/12 mt-4 h-4 rounded-xl bg-gray-600 blur-sm animate-pulse">
+                                    </span>
+                                </div>
+                                <div
+                                    className="mt-4 text-lg text-gray-500 flex flex-row flex-wrap flex-1 gap-1 max-h-[96px] max-w-fit overflow-clip">
+                                    <div
+                                        className="mt-4 rounded-xl bg-gray-600 animate-pulse blur-sm w-40 h-22">
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+                <div
+                    className="absolute inset-0 flex justify-center items-center z-10">
+                    <span className="text-3xl font-bold text-white">Start searching to fill history.</span>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default DefaultViewComponent;
